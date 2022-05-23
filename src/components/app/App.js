a5bd8fd34a276c03f3442d81fdd262e6a14af95f
Vika <viktoriav.aero@gmail.com>
2022-05-23 13:16:14 +0300

    added route for singleComic

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { MainPage, ComicsPage } from "../pages";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { MainPage, ComicsPage, Page404, SingleComicPage } from "../pages";
 import AppHeader from "../appHeader/AppHeader";
 
 const App = () => {
@@ -9,10 +9,20 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route path="/" element={<MainPage/>}/>
-                        <Route path="/comics" element={<ComicsPage/>}/>
-                    </Routes>
+                    <Switch>
+                        <Route exact path="/">
+                            <MainPage />
+                        </Route>
+                        <Route exact path="/comics">
+                            <ComicsPage />
+                        </Route>
+                        <Route exact path="/comics/:comicId">
+                            <SingleComicPage />
+                        </Route>
+                        <Route path="*">
+                            <Page404/>
+                        </Route>
+                    </Switch>
                 </main>
             </div>
         </Router>
